c6764720e2c8fd65ddfcad33af07986f939e227e
DiegoGallardoRP <150666404+DiegoGallardoRP@users.noreply.github.com>
2024-12-09 14:11:31 +0100

    Add files via upload

--- T2/EJ3/buscarRegistro.php
+++ T2/EJ3/buscarRegistro.php
@@ -6,6 +6,51 @@
     }
 
     try{
+		$conexion=new PDO('mysql:host=localhost;dbname=ej2t2;charset=utf8','root','');
+		
+		$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		if(isset($_REQUEST[enviar])){
+			$nombre=htmlspecialchars(trim(strip_tags($_REQUEST['nombre'])),ENT_QUOTES,'UTF-8');
+			$apellidos=htmlspecialchars(trim(strip_tags($_REQUEST['apellidos'])),ENT_QUOTES,'UTF-8');
+			
+			if($nombre!=""&&$apellidos!=""){
+				$resultado=$conexion->prepare('SELECT * FROM personas WHERE nombre=:nombre AND apellidos=:apellidos');
+				$resultado->execute([":nombre"=>$nombre,":apellidos"=>$apellidos]);
+				
+			}elseif($nombre!=""){
+				$resultado=$conexion->prepare('SELECT * FROM personas WHERE nombre=:nombre');
+				$resultado->execute([":nombre"=>$nombre);
+			}elseif($apellidos!=""){
+				$resultado=$conexion->prepare('SELECT * FROM personas WHERE apellidos=:apellidos');
+				$resultado->execute([":apellidos"=>$apellidos]);
+			}else{
+				$resultado=$conexion->prepare('SELECT * FROM personas');
+			}
+			
+			if($resultado->rowCount()==0){
+				print 'No se encontraron registros';
+			}else{
+				print '<table style="border: 2px solid black"><thead>
+						<tr>
+						<th style="border: 2px solid black">Id</th>
+						<th style="border: 2px solid black">Nombre</th>
+						<th style="border: 2px solid black">Apellidos</th>
+						<th style="border: 2px solid black">Dirección</th>
+						<th style="border: 2px solid black">Teléfono</th>
+						</tr>
+						</thead><tbody>';
+				foreach($resutado as $valor){
+				print "<tr>
+					<td>$resultado[id]</td>
+					<td>$resultado[nombre]</td>
+					<td>$resultado[apellidos]</td>
+					<td>$resultado[direccion]</td>
+					<td>$resultado[telefono]</td>
+				</tr>";				
+				}
+			}
+			
+		}else{
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,11 +60,15 @@
     <title>Document</title>
 </head>
 <body>
-    
+   <form action="buscarRegistro.php" method="post">
+		<p>Nombre: <input type="text" name="nombre"></p>
+		<p>Apellidos: <input type="text" name="apellidos"></p>
+		<p><input type="submit" name="enviar" value="Buscar"><input type="reset" value="Resetear"></p>
+   </form>
 </body>
 </html>
 <?php
-    }catch(Exception $e){
+		}}catch(Exception $e){
         print '<p>Error no se puede conectar con la BBDD por \n'.$e->getMessage().'</p>';
     }
 ?>
